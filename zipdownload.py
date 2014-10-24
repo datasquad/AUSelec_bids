368429576f0faa2782facbc9870b0e807eabe2e7
kpetruskevicius <kpetruskevicius@gmail.com>
2014-10-24 23:15:52 +0100

    Zip downloader

--- zipdownload.py
+++ zipdownload.py
@@ -5,30 +5,56 @@ Created on Wed Oct 15 21:03:00 2014
 
 @author: Ralf Becker
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 24 21:49:41 2014
 
-<<<<<<< HEAD
-# te
-=======
-# tester
->>>>>>> dc1a621fabab0862738e86f21d1c92ca96a83225
+@author: kpetruskevicius
+"""
+import os
+import urllib
+from zipfile import *
+import urllib2
+from bs4 import BeautifulSoup
+downloads = []
+url = 'http://www.nemweb.com.au/REPORTS/CURRENT/Yesterdays_Bids_Reports/'
+list_of_links = []
+conn = urllib2.urlopen(url)
+html = conn.read()
+soup = BeautifulSoup(html)
+links = soup.find_all('a')
+for tag in links:
+    link = tag.get('href',None)
+    if link != None:
+        if link not in list_of_links:
+            list_of_links.append("http://www.nemweb.com.au" + link)
+print list_of_links
 
 from time import time
-import urllib.request
 #import subprocess
 
-target = "http://www.nemweb.com.au/REPORTS/CURRENT/Yesterdays_Bids_Reports/PUBLIC_YESTBID_201408170000_20140818040531.zip" # change this to a more useful URL
-
-#wget_start = time()
+target = list_of_links # list of links to zip files
+targeturl = urllib.URLopener()
 #
 #proc = subprocess.Popen(["wget", target])
 #proc.communicate()
 #
-#wget_end = time()
 
-
-url_start = time()
-test = urllib.request.urlretrieve(target, 'testtest.zip')
-url_end = time()
+url_start = time() #wget_start = time()
+for links in target:
+    if links != 'http://www.nemweb.com.au/REPORTS/CURRENT/':
+        if links not in downloads:
+            print downloads
+            targeturl.retrieve(links, links[80:])
+            downloads.append(links)
+            link = links[80:]
+            downloads.append(links)
+            zipf = zipfile.ZipFile(links[80:],"r")
+            zipf.extractall("/Users/kpetruskevicius/Documents/datasquad/AUSelec_bids/unzipped/")
+            print links[80:] + " downloaded"
+    else:
+        print ""
+url_end = time() #wget_end = time()
 
 #print('wget -> {0:6.4f}'.format((wget_end - wget_start)))
 print('urllib.urlretrieve -> {0:6.4f}'.format((url_end - url_start)))
