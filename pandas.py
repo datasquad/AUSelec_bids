3f48a4ec72aeda5b953ad798c84e2d5953de167f
kpetruskevicius <kpetruskevicius@gmail.com>
2014-11-11 22:04:25 +0000

    pandas

--- pandas.py
+++ pandas.py
@@ -5,25 +5,22 @@ Created on Wed Oct 29 02:49:53 2014
 @author: kpetruskevicius
 """
 import csv
-import os
-import urllib
-from zipfile import *
-import urllib2
-from bs4 import BeautifulSoup
-from time import time #import subprocess
 
 reader = csv.reader(open("info.CSV", "rb"), delimiter=',')
-p = csv.writer(open("karolis.CSV", "wb"))
+f = csv.writer(open("karolis.CSV", "wb"))
+c = csv.writer(open("quantities.CSV", "wb"))
 for line in reader:
     if "C" in line:
         date = line[5:6]
         print date
-    elif "I" in line:
+    elif "I" in line and "REBIDEXPLANATION" in line:
         line[3] = "AEMO"
         f.writerow(line)
-    else:
-        if "ENERGY" in line:
+    elif "ENERGY" in line:
             f.writerow(line)
+    else:
+        if "I" in line and "ROCUP" in line:
+            break
             
 import pandas
 data = pandas.read_csv('karolis.CSV')
