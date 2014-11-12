291288353006e39e75b5490aec0962b43d292fcc
kpetruskevicius <kpetruskevicius@gmail.com>
2014-11-12 19:48:24 +0000

    information splitter

--- pandas.py
+++ pandas.py
@@ -10,16 +10,15 @@ import pandas
 reader = csv.reader(open("info.CSV", "rb"), delimiter=',')
 f = csv.writer(open("prices.CSV", "wb"))
 c = csv.writer(open("quantities.CSV", "wb"))
-for line in reader:
-    if "C" in line and "AEMO" in line:
-        date = line[5:6]
-        print date
-    elif line[3] == 5:
-        f.writerow(line)
-    elif line[3] == 3:
-        c.writerow(line)
-    else:
-        break
-
-data = pandas.read_csv('prices.CSV')
+def splitter(source,prices,quantities):
+    for line in source:
+        if "C" in line:
+            date = line[5:6]
+            print date
+        elif line[3] == "5" and line[6] == "ENERGY" or "I" in line and line[3] == "5":
+            prices.writerow(line)
+        elif line[3] == "3" and line[6] == "ENERGY" or "I" in line and line[3] == "3":
+            quantities.writerow(line)
+splitter(reader,f,c)
+data = pandas.read_csv('quantities.CSV')
 print data
