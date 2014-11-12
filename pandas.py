71a19c4a0c150c8f47034d1b8cf87289ee8588c6
unknown <msra2kp4@J-C07GI2G10001.ds.man.ac.uk>
2014-11-12 19:23:59 +0000

    pandas

--- pandas.py
+++ pandas.py
@@ -5,23 +5,21 @@ Created on Wed Oct 29 02:49:53 2014
 @author: kpetruskevicius
 """
 import csv
+import pandas
 
 reader = csv.reader(open("info.CSV", "rb"), delimiter=',')
-f = csv.writer(open("karolis.CSV", "wb"))
+f = csv.writer(open("prices.CSV", "wb"))
 c = csv.writer(open("quantities.CSV", "wb"))
 for line in reader:
-    if "C" in line:
+    if "C" in line and "AEMO" in line:
         date = line[5:6]
         print date
-    elif "I" in line and "REBIDEXPLANATION" in line:
-        line[3] = "AEMO"
+    elif line[3] == 5:
         f.writerow(line)
-    elif "ENERGY" in line:
-            f.writerow(line)
+    elif line[3] == 3:
+        c.writerow(line)
     else:
-        if "I" in line and "ROCUP" in line:
-            break
-            
-import pandas
-data = pandas.read_csv('karolis.CSV')
+        break
+
+data = pandas.read_csv('prices.CSV')
 print data
