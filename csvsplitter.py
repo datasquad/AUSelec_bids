4b090356e0edc95bf1b9f8beb300a5dc1bbba98e
datasquad <ralffbecker@gmail.com>
2014-11-21 13:06:04 +0000

    21 Nov 2014 updated csvsplitter.py

--- csvsplitter.py
+++ csvsplitter.py
@@ -13,23 +13,44 @@ Created on Wed Oct 29 02:49:53 2014
 """
 import csv
 
-gen_names = []
-date = []
 def splitter(source):
     gfd = csv.reader(open(source, "rb"), delimiter=',')
     f = csv.writer(open("prices.CSV", "wb"))
     c = csv.writer(open("quantities.CSV", "wb"))
+    ind = 0
     for line in gfd:
+        
+        if line[0] == "I":
+            ind = ind + 1
+
         if "C" in line:
             date.append(line[5:6])
-        elif line[3] == "5" and line[6] == "ENERGY" or "I" in line and line[3] == "5":
-            f.writerow(line)
-            if line[5] not in gen_names:
-                gen_names.append(line[5])
-        elif line[3] == "3" and line[6] == "ENERGY" or "I" in line and line[3] == "3":
-            c.writerow(line)
+        elif (line[6] == "ENERGY") or (line[6] == "BIDTYPE"):
+            if ind == 1:  # price file
+                f.writerow(line)
+                if line[5] not in gen_names:
+                    gen_names.append(line[5])
+            else:         # quantity file
+                c.writerow(line)
+                
+#        elif line[3] == "5" and line[6] == "ENERGY" or "I" in line and line[3] == "5":
+#            f.writerow(line)
+#            if line[5] not in gen_names:
+#                gen_names.append(line[5])
+#        elif line[3] == "3" and line[6] == "ENERGY" or "I" in line and line[3] == "3":
+#            c.writerow(line)
 
-opf="info.CSV"
+def qtwriter(ui,uv,up):
+    for mn in ui:
+        if uv == mn[5]:
+            up.writerow(mn)
+
+
+
+gen_names = []
+date = []
+
+opf="PUBLIC_YESTBID_201311020000_20131103040529.CSV"
 splitter(opf)
 """def generatornames(source):
     for line in source:
@@ -39,6 +60,9 @@ splitter(opf)
 
 g = csv.reader(open("prices.csv","rb"))
 generatornames(g)"""
+
+# need to reove hastags from DUID codes!!!!1
+
 gen_names.remove("DUID")
 gen_names.remove("W/HOE#1")
 gen_names.remove("W/HOE#2")
@@ -47,19 +71,18 @@ gen_names.remove("W/HOE#2")
 #print readerprices
 dater = str(date)
 datef = dater[3:7] + "_" + dater[8:10] + "_"+ dater[11:13]
-def qtwriter(ui,uv,up):
-    for mn in ui:
-        if uv == mn[5]:
-            up.writerow(mn)
 for i in gen_names:
     readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
     readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-    g =  str(i) + "_" + datef + ".CSV"
-    p = csv.writer(open(g,"wa"))
+    kl = str(i) + "_" + datef
+    g =  str(i) + "_p_" + datef + ".CSV"
+    gq = str(i) + "_q_" + datef + ".CSV"
+    p = csv.writer(open(g,"wb"))
+    pq = csv.writer(open(gq,"wb"))
     readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
     readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
     qtwriter(readerprices,i,p)
-    qtwriter(readerquantities,i,p)
+    qtwriter(readerquantities,i,pq)
     #for t in readerprices:
       #  if i == t[5]:
            # print t
