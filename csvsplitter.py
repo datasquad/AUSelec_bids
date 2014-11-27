a3a9ca56e957e3bfe40f9a2ae2610866869909d7
kpetruskevicius <kpetruskevicius@gmail.com>
2014-11-27 19:56:08 +0000

    explanations added

--- csvsplitter.py
+++ csvsplitter.py
@@ -21,48 +21,45 @@ def splitter(source):
     for line in gfd:
         
         if line[0] == "I":
-            ind = ind + 1
+            ind = ind + 1 #counting instances of "I" as the first value in the line
 
         if "C" in line:
-            date.append(line[5:6])
+            date.append(line[5:6]) #saving date of the main file
         elif (line[6] == "ENERGY") or (line[6] == "BIDTYPE"):
             if ind == 1:  # price file
-                f.writerow(line)
+                f.writerow(line) #writing prices
                 if line[5] not in gen_names:
                     gen_names.append(line[5])
             else:         # quantity file
-                c.writerow(line)
+                c.writerow(line) #writing quantities
                 
 
-def qtwriter(ui,uv,up):
-    for mn in ui:
-        if (uv == mn[5]) or ("I" == mn[0]): # select if generator match or heading line
-            up.writerow(mn)
+def linewriter(sourcesub,gennamesub,outputsub): 
+    for mn in sourcesub: #looping through the source file
+        if (gennamesub == mn[5]) or ("I" == mn[0]): # select if generator match or heading line
+            outputsub.writerow(mn) #write a line if the gen_name is as selected to output file.
 
-folder = os.listdir(os.getcwd())
-for filename in folder:
-    if "PUBLIC_YESTBID" in filename and ".CSV" in filename:
-        gen_names = []
-        date = []
-
-        opf=filename
-        splitter(opf)
-        gen_names.remove("DUID")
-        dater = str(date)
-        datef = dater[3:7] + "_" + dater[8:10] + "_"+ dater[11:13]
-        for i in gen_names:
-            if "/" in i:
-                i = i.replace("/",'')
-            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
-            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-            kl = str(i) + "_" + datef
-            g =  str(i) + "_p_" + datef + ".CSV"
-            gq = str(i) + "_q_" + datef + ".CSV"
-            p = csv.writer(open(g,"wb"))
-            pq = csv.writer(open(gq,"wb"))
-            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
-            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-            qtwriter(readerprices,i,p)
-            qtwriter(readerquantities,i,pq)
+folder = os.listdir(os.getcwd()) #retrieves a list of files in current working directory
+for filename in folder: # looping through every name in the list
+    if "PUBLIC_YESTBID" in filename and ".CSV" in filename: # checking if it's one of the daily files
+        gen_names = [] #creating empty list
+        date = [] #creating empty list
+        splitter(filename) #running the CSV file through the splitter method
+        gen_names.remove("DUID") # removing DUID value from the list of the generator names. This is created in splitters method
+        dater = str(date) #converting the date into a string
+        datef = dater[3:7] + "_" + dater[8:10] + "_"+ dater[11:13] #picking out relevant date parts
+        for i in gen_names: #looping through generator names list without "DUID"
+            if "/" in i: #checking if the generator name does not have any of the prohibited characters.
+                i = i.replace("/",'') #if they do simply changing the name of the generator. Replacing prohibited character with nothing.
+            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',') #opening prices.csv created in splitter method
+            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',') #opening quantities.csv created by splitter method
+            g =  str(i) + "_p_" + datef + ".CSV" #creating a string which will be a name for a price file of the generator.
+            gq = str(i) + "_q_" + datef + ".CSV" #creating a string which will be a name for a quantity file of the generator.
+            p = csv.writer(open(g,"wb")) #creating file for prices
+            pq = csv.writer(open(gq,"wb")) #creatinf file for quantities
+            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',') #reading main file
+            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',') #reading main file
+            linewriter(readerprices,i,p) #using method linewriter
+            linewriter(readerquantities,i,pq) #using method line writer
 
 
--- grapher_loader.py
+++ grapher_loader.py
@@ -9,20 +9,20 @@ import os
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt #http://matplotlib.org/users/pyplot_tutorial.html
-folder = os.listdir(os.getcwd())
+folder = os.listdir(os.getcwd()) #get the list of files in the current working directory
 
 def loader(name,date):
-    for i in folder:
+    for i in folder: #loop through the list of files in the folder
         if name in i and date in i and "p" in i:
-            loader.prices = pandas.read_csv(i)
+            loader.prices = pandas.read_csv(i) #loading price csv into pandas dataframes
         if name in i and date in i and "q" in i:
-            loader.quantity = pandas.read_csv(i)
+            loader.quantity = pandas.read_csv(i) #loading quanitity csv into pandas dataframes
 
 
-loader("TRIBUTE","2014_09_01")
-q0 = loader.quantity.loc[0,'BANDAVAIL1':'BANDAVAIL10']
-p = loader.prices.loc[0,'PRICEBAND1':'PRICEBAND10']
-q1 = loader.quantity.loc[30,'BANDAVAIL1':'BANDAVAIL10']
+loader("VP5","2014_09_29") #choosing calues required
+q0 = loader.quantity.loc[0,'BANDAVAIL1':'BANDAVAIL10'] #defining values required to use .loc[index,column]
+p = loader.prices.loc[0,'PRICEBAND1':'PRICEBAND10'] #defining values required to use .loc[index,column]
+q1 = loader.quantity.loc[30,'BANDAVAIL1':'BANDAVAIL10'] #defining values required to use .loc[index,column]
 
-plt.plot(q0, p, "r--", q1, p, "b-")
+plt.plot(q0, p, "r--", q1, p, "b-") #plotting graphs (x,y,type,x1,x2,type2)
 plt.show()
