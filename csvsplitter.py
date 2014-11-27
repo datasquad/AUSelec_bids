619309d48256aa927edfec5760faa60edbb34a84
kpetruskevicius <kpetruskevicius@gmail.com>
2014-11-27 15:42:47 +0000

    update for splitter to run through all the files in the working directory

--- csvsplitter.py
+++ csvsplitter.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 21 03:48:00 2014
 
-@author: kpetruskevicius
-"""
-
-# -*- coding: utf-8 -*-
 """
 Created on Wed Oct 29 02:49:53 2014
 
 @author: kpetruskevicius
 """
-import csv
 
+
+
+
+
+
+
+import csv
+import os
 def splitter(source):
     gfd = csv.reader(open(source, "rb"), delimiter=',')
     f = csv.writer(open("prices.CSV", "wb"))
@@ -39,39 +39,30 @@ def qtwriter(ui,uv,up):
         if (uv == mn[5]) or ("I" == mn[0]): # select if generator match or heading line
             up.writerow(mn)
 
+folder = os.listdir(os.getcwd())
+for filename in folder:
+    if "PUBLIC_YESTBID" in filename and ".CSV" in filename:
+        gen_names = []
+        date = []
 
-
-gen_names = []
-date = []
-
-opf="info.CSV"
-splitter(opf)
-
-
-# need to reove hastags from DUID codes!!!!1
-
-
-gen_names.remove("DUID")
-#gen_names.remove("W/HOE#1")
-#gen_names.remove("W/HOE#2")
-#readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
-#readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-#print readerprices
-dater = str(date)
-datef = dater[3:7] + "_" + dater[8:10] + "_"+ dater[11:13]
-for i in gen_names:
-    if "/" in i:
-        i = i.replace("/",'')
-    readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
-    readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-    kl = str(i) + "_" + datef
-    g =  str(i) + "_p_" + datef + ".CSV"
-    gq = str(i) + "_q_" + datef + ".CSV"
-    p = csv.writer(open(g,"wb"))
-    pq = csv.writer(open(gq,"wb"))
-    readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
-    readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
-    qtwriter(readerprices,i,p)
-    qtwriter(readerquantities,i,pq)
+        opf=filename
+        splitter(opf)
+        gen_names.remove("DUID")
+        dater = str(date)
+        datef = dater[3:7] + "_" + dater[8:10] + "_"+ dater[11:13]
+        for i in gen_names:
+            if "/" in i:
+                i = i.replace("/",'')
+            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
+            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
+            kl = str(i) + "_" + datef
+            g =  str(i) + "_p_" + datef + ".CSV"
+            gq = str(i) + "_q_" + datef + ".CSV"
+            p = csv.writer(open(g,"wb"))
+            pq = csv.writer(open(gq,"wb"))
+            readerprices = csv.reader(open("prices.CSV","rb"), delimiter=',')
+            readerquantities = csv.reader(open("quantities.CSV","rb"), delimiter=',')
+            qtwriter(readerprices,i,p)
+            qtwriter(readerquantities,i,pq)
 
 
