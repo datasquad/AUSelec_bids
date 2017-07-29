9a6d6fc7312dba0ff5a79af468207714ea294bd9
Ralf Becker <ralffbecker@gmail.com>
2017-07-29 22:05:20 +0100

    Working on adjusting zipdownload_p3.py

--- zipdownload_p3.py
+++ zipdownload_p3.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-""" File to download zip files from internet
+''' File to download zip files from internet
 Created on Wed Oct 15 21:03:00 2014
 
 @author: Ralf Becker
-"""
+'''
 # -*- coding: utf-8 -*-
 
 import os
@@ -18,7 +18,7 @@ print(downloads) # printing all the files
 url = 'http://www.nemweb.com.au/REPORTS/CURRENT/Yesterdays_Bids_Reports/' #url path to the main website
 conn = urlopen(url)
 html = conn.read()
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html,"html")
 links = soup.find_all('a') #find all links
 """ Creating a list of links to look through"""
 for tag in links:
@@ -34,12 +34,12 @@ targeturl = urllib.request.URLopener() #url opener
 #proc.communicate()
 #
 url_start = time() #wget_start = time()
-"""Downloading zip files from the website, checking if they aren't 
+'''Downloading zip files from the website, checking if they aren't 
 already on the computer and extracting them to a new folder unzippe 
 for this step to work you need to make sure to create a new folder unzipped and
 specify your path to this folder. Instead of kpetruskevicius you will have your
 own folder. I think we'll need to try and figure out a way to make it universal
-at some point """
+at some point '''
 
 for links in target: 
     if links != 'http://www.nemweb.com.au/REPORTS/CURRENT/':
