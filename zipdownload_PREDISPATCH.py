4ade07ae9affb1fee43f01846188c0b8d6052cd9
Ralf Becker <ralffbecker@gmail.com>
2017-08-10 23:30:56 +0100

    add zipdownload_PREDIS...

---
+++ zipdownload_PREDISPATCH.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+This is to download zip files from a web directory
+This for PREDISPATCH_SENSTIVITIES
+Author: Ralf Becker
+"""
+import os
+import requests
+import urllib
+import time
+from zipfile import *
+from bs4 import BeautifulSoup
+
+start_time = time.time()
+
+list_of_links = [] #empty list
+os.chdir('E://elec//data//PreDispatch//test')
+downloads = os.listdir(os.getcwd()) #creating a list of files in the current directory
+
+page = requests.get("http://www.nemweb.com.au/REPORTS/ARCHIVE/Predispatch_Sensitivities/")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+links = soup.find_all('a') #find all links
+# Creating a list of links to look through
+for tag in links:
+    link = tag.get('href',None)
+    if link != None:
+        if link not in list_of_links:
+            list_of_links.append("http://www.nemweb.com.au" + link)
+            
+target = list_of_links[123:] # delete all including 2012
+targeturl = urllib.request.URLopener() #url opener
+
+for links in target: 
+    
+    temp_link = links[67:]
+
+    if temp_link not in downloads:
+        targeturl.retrieve(links, temp_link) # zip file download
+        #ZipFile(temp_link,"r").extractall(os.getcwd()+"E:\\elec\\data\\PreDispatch\\test\\temp") #gives predispatch zip
+        #os.remove(links[80:]) #removing file from computer
+        print(temp_link + " downloaded")
+
+hh_run = (time.time() - start_time)/(60)
+print("My program took", hh_run, " minutes to run")          
